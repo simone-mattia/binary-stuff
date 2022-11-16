0e32fce4bb533d4acc5627d4fd708662956f2d6b
simone-mattia <simone.mattia00@gmail.com>
2022-11-16 21:45:19 +0100

    update

--- simple_loader_rust/src/main.rs
+++ simple_loader_rust/src/main.rs
@@ -1,5 +1,9 @@
-mod headers;
+use super::headers::{binary, section, symbol};
+mod headers
 
 fn main() {
     println!("ciao")
+}
+
+fn load_binary(filename: std::string::String, bin: binary::Binary, type: binary::BinaryType) {
 }
